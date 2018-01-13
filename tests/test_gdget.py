81327a72b7425fae4e86aedc89710a1b3d9d0856
v1shwa <v1shwa@users.noreply.github.com>
2018-01-13 21:37:05 +0530

    [tests] Wait for command to finish.

--- tests/test_gdget.py
+++ tests/test_gdget.py
@@ -5,38 +5,39 @@ import os.path
 import warnings
 
 class TestGdGet(unittest.TestCase):
-    def setUp(self):
-        try:
-            warnings.simplefilter("ignore", ResourceWarning)
-        except NameError:
-            pass
+
+    def get_command_output(self, command):
+        p = popen(command, stdout=PIPE, stderr=PIPE)
+        p.wait()
+        (out, err) = p.communicate()  
+        return err
 
     def test_by_id(self):
-        output = popen(['gdget', '1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', '1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0'])
         self.assertTrue('blur.txt' in str(output) and os.path.isfile('blur.txt'))
 
     def test_by_url_1(self):
-        output = popen(['gdget', 'https://drive.google.com/file/d/1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0/view?usp=sharing'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', 'https://drive.google.com/file/d/1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0/view?usp=sharing'])
         self.assertTrue('blur.txt' in str(output) and os.path.isfile('blur.txt'))
 
     def test_by_url_2(self):
-        output = popen(['gdget', 'https://drive.google.com/uc?id=1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0&export=download'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', 'https://drive.google.com/uc?id=1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0&export=download'])
         self.assertTrue('blur.txt' in str(output) and os.path.isfile('blur.txt'))
 
     def test_by_url_3(self):
-        output = popen(['gdget', 'https://drive.google.com/uc?export=download&confirm=gege&id=1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', 'https://drive.google.com/uc?export=download&confirm=gege&id=1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0'])
         self.assertTrue('blur.txt' in str(output) and os.path.isfile('blur.txt'))
 
     def test_output_flag(self):
-        output = popen(['gdget', 'https://drive.google.com/file/d/1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0/view?usp=sharing','-O','testing_output_flag.txt'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', 'https://drive.google.com/file/d/1sOlEsawO0_YwM4KjKHX_sLB8vijMpcP0/view?usp=sharing','-O','testing_output_flag.txt'])
         self.assertTrue('testing_output_flag.txt' in str(output) and os.path.isfile('testing_output_flag.txt'))
 
     def test_invalid_doc(self):
-        output = popen(['gdget', 'https://drive.google.com/file/d/0BwNnXVsA1pABbDB6LTQwU3l5NW8/view?usp=sharing'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', 'https://drive.google.com/file/d/0BwNnXVsA1pABbDB6LTQwU3l5NW8/view?usp=sharing'])
         self.assertTrue('Unable to download' in str(output))
 
     def test_invalid_url(self):
-        output = popen(['gdget', 'https://google.com'], stdout=PIPE, stderr=PIPE).stderr.read()
+        output = self.get_command_output(['gdget', 'https://google.com'])
         self.assertTrue('Not a valid Google drive link' in str(output))
 
 if __name__ == '__main__':
